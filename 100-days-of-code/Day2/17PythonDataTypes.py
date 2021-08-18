3b6b464f9987d299b6ad2c45afcfe42cd6d4a530
emil <emil.szczesniak@pollub.edu.pl>
2021-08-18 23:15:30 +0200

    Day2

---
+++ 100-days-of-code/Day2/17PythonDataTypes.py
@@ -0,0 +1,17 @@
+"""Data types:
+1. Strings
+for example:
+"""
+print("hello"[0])
+print("123" + "345")
+
+"""2. Integer"""
+print(123 + 345)
+
+#very larg Integer
+#123_456_789
+print(123_345 + 67)
+
+"""3.Float"""
+print(3.14323)
+"""4. Boolean -> True False"""
