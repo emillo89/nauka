e38ddc54d3e87db271884bfe81c020b8ced1d9c9
emil <emil.szczesniak@pollub.edu.pl>
2021-08-19 15:24:59 +0200

    Number manipulation and f-string

---
+++ 100-days-of-code/Day2/22NumberManipulationAndFSring.py
@@ -0,0 +1,27 @@
+#1.round with decimal place
+print(round(8 / 3, 2))
+
+#2.floor division
+print(8 // 3)
+
+#3. Manipulation number:
+"""
++=
+-=
+*=
+/=
+
+for example:
+"""
+score = 0
+print("score = ",score)
+
+score += 1
+print("score = ", score)
+
+"""f-string"""
+score = 2
+height = 2.1
+isWinning = True
+
+print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
