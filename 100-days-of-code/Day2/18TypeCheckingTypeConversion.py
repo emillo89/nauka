a57bd55d3e88c9f0c101c976a5d831e9565bc7bf
emil <emil.szczesniak@pollub.edu.pl>
2021-08-19 07:11:18 +0200

    Type checking and type conversion

---
+++ 100-days-of-code/Day2/18TypeCheckingTypeConversion.py
@@ -0,0 +1,19 @@
+# num_char = len(input("What is your name? "))
+# new_num_char = str(num_char)
+
+# Concatenation the same type:
+# print("Your name has " + new_num_char + " charakters")
+
+# checking type
+a = 123
+print(type(a))
+
+#What hapeen?
+b = float(123)
+print(type(b))
+
+#Results float
+print(70 + float("100.5"))
+
+#Results str
+print(str(70) + str(100))
