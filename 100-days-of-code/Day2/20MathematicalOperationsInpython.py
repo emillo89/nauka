ef8ccc3c84f6beca0f103289f9c842db568f3e89
emil <emil.szczesniak@pollub.edu.pl>
2021-08-19 09:32:53 +0200

    Mathematical operations

---
+++ 100-days-of-code/Day2/20MathematicalOperationsInpython.py
@@ -0,0 +1,20 @@
+#1. addition
+print(3 + 5)
+#2. subtraction
+print(7 - 3)
+#3. multiplication
+print(3 * 2)
+#4. division
+print(6 / 2)
+#5. power
+print(3 ** 2)
+
+"""PEMDAS - Parentheses Exponents Multiplication Division Addition Subtraction
+()
+**
+* /
++ - """
+
+#for example
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
