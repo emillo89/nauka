f3f72442198185ec04a10e12539f91f38fdf5de9
emil <emil.szczesniak@pollub.edu.pl>
2021-08-20 06:40:14 +0200

    Day3

---
+++ 100-days-of-code/Day3/27.ControlFlowWithIfElseAndControlOperators.py
@@ -0,0 +1,16 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride")
+
+
+"""Comparison operators:
+> greater than
+< less then
+>= greater than or equal to 
+<= less than or equal to
+== equal to
+!= not equal to"""
