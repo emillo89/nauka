d7623e1aa2d14db36bfd5f694d4c1cd536720777
emil <emil.szczesniak@pollub.edu.pl>
2021-08-28 13:58:05 +0200

    Day 4

---
+++ 100-days-of-code/Day4/39. RandomModule.py
@@ -0,0 +1,14 @@
+import random
+from my_module import pi
+
+
+randomInteger = random.randint(1,100)
+print(randomInteger)
+print(pi)
+
+
+randomFloat = random.random() * 5
+print(randomFloat)
+
+love_score = random.randint(1, 100)
+print(f'Your love score is {love_score}')
