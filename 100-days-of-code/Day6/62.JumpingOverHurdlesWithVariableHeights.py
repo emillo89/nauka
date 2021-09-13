43defa3849512fb98bdbd1c8d6f70b6f6dc1f80f
emil <emil.szczesniak@pollub.edu.pl>
2021-09-13 23:09:02 +0200

    Jumping over Hurdles4

---
+++ 100-days-of-code/Day6/62.JumpingOverHurdlesWithVariableHeights.py
@@ -0,0 +1,48 @@
+"""
+exercise
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+"""
+
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def square():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+
