2c4c72578e05c5fac1e3661082f63f82a4d3437e
emil <emil.szczesniak@pollub.edu.pl>
2021-09-19 06:08:18 +0200

    Day8

---
+++ 100-days-of-code/Day8/79.FunctionsWithInputs.py
@@ -0,0 +1,19 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+#
+
+#function what allows for input
+
+
+
+def greet_with_name(name):
+    print('My functions')
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name('Emil')
+
+#def grret_with_name(name='Emil') name = jest parametrem, Emil = argumentem
