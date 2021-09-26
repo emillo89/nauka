598fd01bb34dde76a62da7772e3d3b05a77b9e17
emil <emil.szczesniak@pollub.edu.pl>
2021-09-26 15:13:13 +0200

    Calculator finishing with a fixes bug

---
+++ 100-days-of-code/Day10/104.CalculatorFinishingTouchesAndBugFixes.py
@@ -0,0 +1,61 @@
+from art import logo
+
+
+#Calculator
+
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Substract
+def substract(n1, n2):
+    return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+#Divide
+def divide(n1, n2):
+    try:
+        return n1 / n2
+    except ZeroDivisionError:
+        return None
+
+
+operations = {
+    '+' : add,
+    '-' : substract,
+    '*' : multiply,
+    '/' : divide
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number? "))
+
+
+    for key in operations:
+        print(key)
+
+    question = True
+
+    while question:
+
+        operation_symbol = input("Pick an operation: ")
+
+        num2 = float(input("What's the next number? "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        your_choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit, or 'i' to start new calculation: ")
+        if your_choice == 'y':
+            num1 = answer
+        elif your_choice == 'i':
+            calculator()
+        else:
+            question = False
+
+calculator()
