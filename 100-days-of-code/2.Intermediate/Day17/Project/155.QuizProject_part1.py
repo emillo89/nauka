01a6562d481d0140a28eaf47135489a9bb6a4ffa
emil <emil.szczesniak@pollub.edu.pl>
2021-10-07 22:45:43 +0200

    Completed Project

--- 100-days-of-code/2.Intermediate/Day17/Project/155.QuizProject_part1.py
+++ 100-days-of-code/2.Intermediate/Day17/Project/155.QuizProject_part1.py
@@ -14,3 +14,6 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_question():
     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final scire was: {quiz.score}/{len(question_bank)}")
