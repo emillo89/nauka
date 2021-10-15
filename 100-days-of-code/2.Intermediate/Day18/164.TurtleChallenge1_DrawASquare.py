7445dce4c52b36ae7333f590e023b00206a3ff44
emil <emil.szczesniak@pollub.edu.pl>
2021-10-15 13:21:11 +0200

    Draw a square

---
+++ 100-days-of-code/2.Intermediate/Day18/164.TurtleChallenge1_DrawASquare.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+timmy.shape("turtle")
+timmy.color("gold")
+
+for _ in range(4):
+    timmy.forward(100)
+    timmy.right(90)
+
+screen = Screen()
+screen.exitonclick()
