b2899741038e7a18f48d54e0af43a9eac3de73dc
emil <emil.szczesniak@pollub.edu.pl>
2021-10-15 13:21:47 +0200

    Modules

---
+++ 100-days-of-code/2.Intermediate/Day18/165.Importing_modules.py
@@ -0,0 +1,21 @@
+
+"""1 way"""
+from turtle import Turtle
+tom = Turtle()
+
+"""2 way"""
+import turtle
+tur = turtle.Turtle()
+
+"""3 way"""
+from turtle import *
+tonny = Turtle()
+
+"""Aliasing modules"""
+import turtle as t
+
+tim = t.Turtle()
+
+"""Installing Packages"""
+import heroes
+print(heroes.gen())
