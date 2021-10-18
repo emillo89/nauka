a457a7718a673139e003373d304a5ae13296f343
emil <emil.szczesniak@pollub.edu.pl>
2021-10-18 20:02:11 +0200

    Detect collision with food

---
+++ 100-days-of-code/2.Intermediate/Day21/main.py
@@ -0,0 +1,34 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+
+
+# set width, height, bgcolor
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake by Emil")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_in_on = True
+while game_in_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detective collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+
+screen.exitonclick()
