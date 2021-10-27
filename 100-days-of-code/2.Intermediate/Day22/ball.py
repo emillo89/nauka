a865a71a9fcdc9d1d8408cdafe30c7a9cbf96749
emil <emil.szczesniak@pollub.edu.pl>
2021-10-27 22:14:58 +0200

    Ball move

--- 100-days-of-code/2.Intermediate/Day22/ball.py
+++ 100-days-of-code/2.Intermediate/Day22/ball.py
@@ -9,8 +9,13 @@ class Ball(Turtle):
         self.shape('circle')
         self.shapesize(stretch_wid=1, stretch_len=1)
         self.goto(x=0, y=0)
+        self.x_cor = 10
+        self.y_cor = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_cor
+        new_y = self.ycor() + self.y_cor
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_cor *= -1
