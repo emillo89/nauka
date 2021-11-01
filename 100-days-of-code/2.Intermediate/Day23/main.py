8ffee60f78b770095f6299d035ce9af6b3fce91c
emil <emil.szczesniak@pollub.edu.pl>
2021-11-01 19:59:59 +0100

    Car and Player

--- 100-days-of-code/2.Intermediate/Day23/main.py
+++ 100-days-of-code/2.Intermediate/Day23/main.py
@@ -1,11 +1,22 @@
 import time
 from turtle import Screen
+from car_manager import CarManager
+from player import Player
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+player = Player()
+screen.onkey(fun=player.move, key='Up')
+
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
-    screen.update()
+    for i in range(10):
+        car = CarManager()
+        time.sleep(0.1)
+        screen.update()
+
+
+screen.exitonclick()
