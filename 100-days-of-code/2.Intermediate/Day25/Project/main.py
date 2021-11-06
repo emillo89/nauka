b7113ffb396d5d195ff5f3e1e24223f25361182d
emil <emil.szczesniak@pollub.edu.pl>
2021-11-06 23:21:35 +0100

    Full project Quiz Game

--- 100-days-of-code/2.Intermediate/Day25/Project/main.py
+++ 100-days-of-code/2.Intermediate/Day25/Project/main.py
@@ -28,19 +28,26 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
-
     if answer_state in all_states :
         # Create a turtle to write the name of the state at the state's x and y coordate
         create_turtle(answer_state,states_data)
         guessed_states.append(answer_state)
+    elif answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
 
 
 
-def get_mouse_click_coor(x, y):
-    print(x,y)
-
-turtle.onscreenclick(get_mouse_click_coor)
-
-turtle.mainloop()
+# def get_mouse_click_coor(x, y):
+#     print(x,y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+#
+# turtle.mainloop()
 
