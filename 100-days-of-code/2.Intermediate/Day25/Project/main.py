0ac7c9a358d243ee117e9cef25acad2a158f369d
emil <emil.szczesniak@pollub.edu.pl>
2021-11-07 10:36:40 +0100

    List comprehension in project 25

--- 100-days-of-code/2.Intermediate/Day25/Project/main.py
+++ 100-days-of-code/2.Intermediate/Day25/Project/main.py
@@ -33,10 +33,12 @@ while len(guessed_states) < 50:
         create_turtle(answer_state,states_data)
         guessed_states.append(answer_state)
     elif answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        #Use a list comprehension
+        missing_states = [state for state in all_states if state not in guessed_states]
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
