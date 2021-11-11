375882f0901be8c3d5566e8c279be09a50196970
emil <emil.szczesniak@pollub.edu.pl>
2021-11-11 09:15:49 +0100

    Converter mile to kilometers Project

--- 100-days-of-code/2.Intermediate/Day27/251_tkinker_layout_managers.py
+++ 100-days-of-code/2.Intermediate/Day27/251_tkinker_layout_managers.py
@@ -6,7 +6,7 @@ window.minsize(width=500, height=300)
 window.config(padx=50, pady=200)
 
 #Label
-my_label = tkinter.Label(text="I am a Label", font = ("Arial", 24, "bold"))
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
 # my_label.place(x=100, y=0)
 my_label.grid(column=0, row=0)
 my_label.config(padx=50, pady=50)
@@ -17,11 +17,13 @@ my_label.config(text="inny")
 
 #Button
 
+
 def button_click():
     new_text = input.get()
-    my_label.config(text = new_text)
+    my_label.config(text=new_text)
 
-button = tkinter.Button(text="Click Me", command= button_click )
+
+button = tkinter.Button(text="Click Me", command=button_click)
 button.grid(column=1, row=1)
 
 #New Button
@@ -31,7 +33,7 @@ new_button.grid(column=2, row=0)
 #Entry
 
 input = tkinter.Entry(width=10)
-input.grid(column=3, row=2 )
+input.grid(column=3, row=2)
 
 
 window.mainloop()
---
+++ 100-days-of-code/2.Intermediate/Day27/252_mile_to_kilometers_converter_project.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.config(padx=20, pady=20)
+
+#Labels
+is_equal_to = Label(text="is equal to", font=("Arial", 14, "bold"))
+is_equal_to.grid(row=1, column=0)
+
+miles = Label(text="Miles", font=("Arial", 14, "bold"))
+miles.grid(row=0, column=2)
+
+km = Label(text="Km", font=("Arial", 14, "bold"))
+km.grid(row=1, column=2)
+
+convert = Label(text="0",font=("Arial", 14, "bold"))
+convert.grid(row=1, column=1)
+
+#Entry
+entry = Entry(width=15)
+entry.grid(row=0, column=1)
+
+
+#Button
+def calculator():
+    calc = entry.get()
+    calculate = float(calc) * 1.609
+    convert.config(text=f"{calculate}")
+
+
+
+button = Button(text="Calculate",font=("Arial", 14, "bold"), command=calculator)
+button.grid(row=2, column=1)
+
+
+window.mainloop()
