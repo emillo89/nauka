e3215023b9b4f7f1eaede67c6841b9dbd8070733
emil <emil.szczesniak@pollub.edu.pl>
2021-11-28 22:42:41 +0100

    Birthday wisher project

---
+++ 100-days-of-code/2.Intermediate/Day32/292automated_birthday_wisher_project.py
@@ -0,0 +1,30 @@
+import pandas
+import datetime as dt
+import random
+import smtplib
+
+my_email = "appbrewery5@gmail.com"
+password = "testowe1234"
+
+# current day and month
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
+
+# read pandas csv file
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(row.month, row.day) : row for index, row in data.iterrows()}
+if today_tuple in birthday_dict:
+    number = random.randint(1, 3)
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{number}.txt"
+    with open(file_path) as letter_file:
+        content = letter_file.read()
+        content = content.replace("[NAME]", birthday_person["name"])
+    print(content)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday\n\n {content}")
