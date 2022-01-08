cab7b33504ec390ca417927c546a870c74e993c2
emil <emil.szczesniak@pollub.edu.pl>
2022-01-08 10:29:33 +0100

    441 follow all the followers

--- 100-days-of-code/2.Intermediate/Day52/437_instagram_project.py
+++ 100-days-of-code/2.Intermediate/Day52/437_instagram_project.py
@@ -2,6 +2,7 @@ import os
 import time
 
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
@@ -11,12 +12,14 @@ INSTAGRAM_EMAIL = os.environ["MY_EMAIL"]
 INSTAGRAM_PASSWORD = os.environ["MY_PASSWORD"]
 URL = "https://www.instagram.com/"
 
+
 class InstaFollower:
 
     def __init__(self, driver_path):
         self.driver = webdriver.Chrome(executable_path=driver_path)
 
     def login(self):
+        """Function takes your email, password and login on your account."""
         self.driver.get(URL)
         time.sleep(2)
         cookie = self.driver.find_element(By.XPATH, "/html/body/div[4]/div/div/button[1]")
@@ -36,21 +39,33 @@ class InstaFollower:
         notification = self.driver.find_element(By.XPATH, "/html/body/div[6]/div/div/div/div[3]/button[2]")
         notification.click()
 
-
     def find_followers(self):
+        """Function find all followers in your choice location"""
         time.sleep(2)
         self.driver.get(f"{URL}{SIMILAR_ACCOUNT}/")
-        followers = self.driver.find_element(By.XPATH, "//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a")
+        followers = self.driver.find_element(By.XPATH, "//*[@id='react-root']/section/main/div/header/"
+                                                       "section/ul/li[2]/a")
         followers.click()
         time.sleep(2)
         scroll_down = self.driver.find_element(By.XPATH, "/html/body/div[6]/div/div/div[2]")
 
-        for i in range(5):
+        for i in range(2):
             self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scroll_down)
             time.sleep(1)
 
     def follow(self):
-        pass
+        """Function adds all followers to your follows"""
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, "li div button")
+        for button in all_buttons:
+            try:
+                button.click()
+                time.sleep(2)
+
+            except ElementClickInterceptedException:
+                cancel = self.driver.find_element(By.XPATH, "/html/body/div[7]/div/div/div/div[3]/button[2]")
+                cancel.click()
+                time.sleep(1)
+
 
 insta = InstaFollower(CHROME_DRIVER_PATX)
 insta.login()
