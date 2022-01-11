fc9c45f233a91e8d33da209dc9d7abd3095f9528
emil <emil.szczesniak@pollub.edu.pl>
2022-01-11 08:15:04 +0100

    455 Challenge with decorators

---
+++ 100-days-of-code/2.Intermediate/Day55/455_challenge_1.py
@@ -0,0 +1,54 @@
+from flask import Flask
+# from  markupsafe import escape
+
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def hello_world():
+    #add few line to your code with style
+    return '<h1 style="text-align: center">Hello, world!</h1>' \
+           '<p>This is a paragraph</p>' \
+           '<img src="https://th.bing.com/th/id/R.77b1d50bbaf24aff4f95cd904b4e0eee?rik=KjFHBChfLdxaOw&riu=http%3a%2f%2fzoomia.pl%2ffiles%2fi%2f19%2f51%2fkoty-sprzedam-brytyjskie_big21611_19513082991331497997.jpg&ehk=n6miu7CiLV%2bc3zFDrATfEZoSS%2bukX1bEwgk5hqAjjqQ%3d&risl=&pid=ImgRaw&r=0", width=200>'
+
+
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}</b>"
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return f"<em>{function()}</em>"
+    return wrapper
+
+
+def make_underline(function):
+    def wrapper():
+        return f"<u>{function()}</u>"
+    return wrapper
+
+
+#Different routes using the app.route decorator
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello the {name} you are {number} years old."
+
+#creating variable paths and converting the path to a specified data
+
+
+
+@app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underline
+def bye():
+    return "Bye"
+
+
+if __name__ == "__main__":
+    #Run the app in debug mode to auto-reload
+    app.run(debug=True)
