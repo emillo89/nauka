f7afbfa82d1f4375965e48598c6304c110043476
emil <emil.szczesniak@pollub.edu.pl>
2022-01-11 06:44:18 +0100

    453 add comments

--- 100-days-of-code/2.Intermediate/Day55/453_working_flask_url_path.py
+++ 100-days-of-code/2.Intermediate/Day55/453_working_flask_url_path.py
@@ -7,13 +7,16 @@ print(__name__)
 def hello_world():
     return "Hello, world!"
 
+#Different routes using the app.route decorator
 @app.route("/bye")
 def bye():
     return "Bye"
 
+#creating variable paths and converting the path to a specified data type
 @app.route("/username/<name>/<int:number>")
 def greet(name, number):
     return f"Hello the {name} you are {number} years old."
 
 if __name__ == "__main__":
+    #Run the app in debug mode to auto-reload
     app.run(debug=True)
