d8759985884e376c3ba14d38d2510901cbd11c93
emil <emil.szczesniak@pollub.edu.pl>
2022-01-16 13:11:41 +0100

    487 Challenge 1

--- 100-days-of-code/2.Intermediate/Day49/__init__.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-
-service = Service(executable_path="C:/Users/emils/PycharmProjects/Development/chromedriver.exe")
-driver = webdriver.Chrome(service=service)
-driver.get("http://secure-retreat-92358.herokuapp.com/")
-
