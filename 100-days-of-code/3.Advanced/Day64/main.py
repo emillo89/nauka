4271c27c171548a47204429d264044ca93fb597a
emil <emil.szczesniak@pollub.edu.pl>
2022-02-03 22:58:34 +0100

    537 View Movie List Items

---
+++ 100-days-of-code/3.Advanced/Day64/main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+# create DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#Create table
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(256), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(256), nullable=True)
+    img_url = db.Column(db.String(256), nullable=False)
+
+db.create_all()
+
+#After adding the new_movie code needs to be commented, not trying to add the same movie twice
+# new_movie = Movies(
+#         title="Phone Booth",
+#         year=2002,
+#         description="Publicist Stuart Shepeard finds himself trapped in a phone",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#
+#     )
+#
+# db.session.add(new_movie)
+# db.session.commit()
+
+@app.route("/")
+def home():
+    all_movies = Movies.query.all()
+    print(all_movies)
+    return render_template("index.html", movies=all_movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
