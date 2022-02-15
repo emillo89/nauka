0be5c0e1a9f0e12b9ccc2f4d8d959887ff220598
emil <emil.szczesniak@pollub.edu.pl>
2022-02-15 21:34:40 +0100

    564. Requirements 3 - Edit Blog Posts.

--- 100-days-of-code/3.Advanced/Day67/main.py
+++ 100-days-of-code/3.Advanced/Day67/main.py
@@ -88,5 +88,26 @@ def new_post():
     return render_template('make-post.html', post=post)
 
 
+@app.route('/edit-post/<int:post_id>', methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        post.date = date.today().strftime("%B, %d, %Y")
+        db.session.commit()
+        return redirect(url_for('show_post', post_id=post.id))
+    return render_template('make-post.html', post=edit_form)
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
