90bf842f141d6440833d139818ad7e6e2f2b0e2a
emil <emil.szczesniak@pollub.edu.pl>
2022-02-15 20:29:59 +0100

    563.Requirement 2- Post a New Blog Post.

--- 100-days-of-code/3.Advanced/Day67/main.py
+++ 100-days-of-code/3.Advanced/Day67/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
+from datetime import date
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -38,7 +39,7 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+    body = CKEditorField("Body", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
@@ -66,6 +67,26 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+#add new post
+@app.route('/new-post', methods=["GET","POST"])
+def new_post():
+    post = CreatePostForm()
+    if post.validate_on_submit():
+        new_post = BlogPost(
+            title=post.title.data,
+            subtitle=post.subtitle.data,
+            date=date.today().strftime("%B, %d, %Y"),
+            author=post.author.data,
+            img_url=post.img_url.data,
+            body=post.body.data,
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+    return render_template('make-post.html', post=post)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
