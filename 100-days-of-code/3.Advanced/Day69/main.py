f76ed47509ba931c9e1284064b5f3b7b2c2a9d73
emil <emil.szczesniak@pollub.edu.pl>
2022-02-20 16:00:14 +0100

    Day 71.

--- 100-days-of-code/3.Advanced/Day69/main.py
+++ 100-days-of-code/3.Advanced/Day69/main.py
@@ -1,5 +1,5 @@
 from functools import wraps
-
+import os
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -221,5 +221,7 @@ def delete_post(post_id):
     return redirect(url_for('get_all_posts'))
 
 
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=port)
