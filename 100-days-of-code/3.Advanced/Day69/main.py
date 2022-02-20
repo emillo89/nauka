b14aeac786ebfe3d8e46a294c63ede547112ed96
emil <emil.szczesniak@pollub.edu.pl>
2022-02-20 21:27:25 +0100

    597. Highest and lowest earning degress.

--- 100-days-of-code/3.Advanced/Day69/main.py
+++ 100-days-of-code/3.Advanced/Day69/main.py
@@ -12,7 +12,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
