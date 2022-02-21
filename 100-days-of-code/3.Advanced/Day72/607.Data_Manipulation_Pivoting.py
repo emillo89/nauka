42661db1c249909cdc7ee4b9fc9cf2bc8efaeb7a
emil <emil.szczesniak@pollub.edu.pl>
2022-02-21 17:10:55 +0100

    Full day 72

--- 100-days-of-code/3.Advanced/Day72/607.Data_Manipulation_Pivoting.py
+++ 100-days-of-code/3.Advanced/Day72/607.Data_Manipulation_Pivoting.py
@@ -35,59 +35,53 @@ import pandas as pd
 
 #Read the .csv file 
 df = pd.read_csv('QueryResults.csv', names=['DATE','TAG','POSTS'], header=0)
-df
+print(df)
 
 """**Challenge**: Examine the first 5 rows and the last 5 rows of the of the dataframe"""
 
 #the first 5 rows and the last 5 rows of the of the dataframe
-df.head()
-df.tail()
+print(df.head())
+print(df.tail())
 
 """**Challenge:** Check how many rows and how many columns there are. 
 What are the dimensions of the dataframe?
 """
 
 #how many rows and how many columns
-df.shape
+print(df.shape)
 
 """**Challenge**: Count the number of entries in each column of the dataframe"""
 
 #Count the number of entries in each column of the dataframe
-df.count()
+print(df.count())
 
 """**Challenge**: Calculate the total number of post per language.
 Which Programming language has had the highest total number of posts of all time?
 """
 
 #Calculate the total number of post per language
-df.groupby('TAG').sum()
+print(df.groupby('TAG').sum())
 
-"""Some languages are older (e.g., C) and other languages are newer (e.g., Swift). The dataset starts in September 2008.
+"""Some languages are older (e.g., C) and other languages are newer (e.g., Swift). 
+The dataset starts in September 2008.
 
-**Challenge**: How many months of data exist per language? Which language had the fewest months with an entry? 
-
+**Challenge**: How many months of data exist per language? 
+Which language had the fewest months with an entry? 
 """
-
 #How many months of data exist per language
-df.groupby('TAG').count()
+print(df.groupby('TAG').count())
 
 """## Data Cleaning
-
-Let's fix the date format to make it more readable. We need to use Pandas to change format from a string of "2008-07-01 00:00:00" to a datetime object with the format of "2008-07-01"
-"""
-
-df['DATE'][1]
+Let's fix the date format to make it more readable. We need to use Pandas to change format from a string of "2008-07-01 00:00:00" 
+to a datetime object with the format of "2008-07-01"""
+print(df['DATE'][1])
 #the same method df.DATE[1]
 
 #convert entire column (string to datetime)
 df.DATE = pd.to_datetime(df.DATE)
+print(df.head())
 
-df.head()
-
-"""## Data Manipulation
-
-
-"""
+"""## Data Manipulation"""
 
 test_df = pd.DataFrame({'Age':['Young', 'Young', 'Young', 'Young', 'Old', 'Old', 'Old', 'Old'],
                         'Actor': ['Jack', 'Arnold', 'Keanu', 'Sylvester', 'Jack', 'Arnold', 'Keanu', 'Sylvester'],
@@ -95,8 +89,7 @@ test_df = pd.DataFrame({'Age':['Young', 'Young', 'Young', 'Young', 'Old', 'Old',
 
 #pivoting DataFrame example
 pivot_df = test_df.pivot(index='Age', columns='Actor', values='Power')
-
-pivot_df
+print(pivot_df)
 
 """**Challenge**: What are the dimensions of our new dataframe? How many rows and columns does it have? Print out the column names and print out the first 5 rows of the dataframe."""
 
@@ -104,52 +97,29 @@ pivot_df
 reshaped_df = df.pivot(index='DATE', columns='TAG', values='POSTS')
 
 #show dataframe
-reshaped_df
+print(reshaped_df)
 
 #count rows and columns
-reshaped_df.shape
+print(reshaped_df.shape)
 
 #column names 
-reshaped_df.columns
+print(reshaped_df.columns)
 
 #the first 5 rows of the dataframe.
-reshaped_df.head()
+print(reshaped_df.head())
 
 """**Challenge**: Count the number of entries per programming language. Why might the number of entries be different? """
 
 #Count the number of entries per programming language. Why might the number of entries be different?
-reshaped_df.count()
-
-reshaped_df.head()
+print(reshaped_df.count())
+print(reshaped_df.head())
 
 #replace Nan values to 0
 reshaped_df.fillna(0, inplace=True)
-
-reshaped_df.head()
+print(reshaped_df.head())
 
 #check if there are any NaN values
-reshaped_df.isna().values.any()
-
-"""## Data Visualisaton with with Matplotlib
-
-**Challenge**: Use the [matplotlib documentation](https://matplotlib.org/3.2.1/api/_as_gen/matplotlib.pyplot.plot.html#matplotlib.pyplot.plot) to plot a single programming language (e.g., java) on a chart.
-"""
-
-
-
-
-
-
-
-"""**Challenge**: Show two line (e.g. for Java and Python) on the same chart."""
-
-
-
-"""# Smoothing out Time Series Data
-
-Time series data can be quite noisy, with a lot of up and down spikes. To better see a trend we can plot an average of, say 6 or 12 observations. This is called the rolling mean. We calculate the average in a window of time and move it forward by one overservation. Pandas has two handy methods already built in to work this out: [rolling()](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.rolling.html) and [mean()](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.core.window.rolling.Rolling.mean.html). 
-"""
-
+print(reshaped_df.isna().values.any())
 
 
 
