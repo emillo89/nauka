1f0e803c84c56cb0d9500f7410800c221760e540
emil <emil.szczesniak@pollub.edu.pl>
2022-03-13 06:04:30 +0100

    649. Investigate the Films that had Zero Revenue.

--- 100-days-of-code/3.Advanced/Day77/Day77.py
+++ 100-days-of-code/3.Advanced/Day77/Day77.py
@@ -1,6 +1,7 @@
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-import numpy as pd
+import pandas as pd
+import matplotlib.pyplot as plt
 
 pd.options.display.float_format = '{:,.2f}'.format
 
@@ -47,3 +48,42 @@ print(data.info())
 
 data['Release_Date'] = pd.to_datetime(data['Release_Date'])
 print(data.head())
+
+"""Convert the `Release_Date` column to a Pandas Datetime type. """
+
+#Convert the Release_Date column to a Pandas Datetime type
+print(data.info())
+
+data['Release_Date'] = pd.to_datetime(data['Release_Date'])
+print(data.head())
+
+"""Descriptive Statistics"""
+
+#average production budget of the films [mean]
+print(data.describe())
+
+#average worldwide gross revenue of films [mean]
+print(data.describe())
+
+#minimums for worldwide and domestic revenue
+print(data[data.USD_Production_Budget == 1100])
+
+print(data[(data.USD_Production_Budget < 5000000) & (data.USD_Worldwide_Gross > data.USD_Production_Budget)])
+
+#highest production budget and highest worldwide gross revenue of any film
+print(data[data.USD_Production_Budget == 425000000])
+
+"""Investigating the Zero Revenue Films"""
+
+#How many films grossed $0 domestically (i.e., in the United States)? What were the highest budget films that grossed nothing
+print(data[data.USD_Domestic_Gross == 0].count())
+
+"""**Challenge**: How many films grossed $0 worldwide? What are the highest budget films that had no revenue internationally?"""
+
+#What were the highest budget films that grossed nothing?
+zero_domestic = data[data.USD_Domestic_Gross == 0]
+print(zero_domestic.sort_values('USD_Production_Budget', ascending=False))
+
+#No worldwide revenue
+zero_worldwide = data[data.USD_Worldwide_Gross == 0]
+print(zero_worldwide.sort_values('USD_Production_Budget', ascending=False))
